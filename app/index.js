440e2d2e04954c3c5e41aee3159dc7faca1e9b75
alexjtucker <alexjtucker@gmail.com>
2016-03-25 21:48:39 -0400

    FriendsContainer Adds friend list parent and children to perform operations.

--- app/index.js
+++ app/index.js
@@ -17,7 +17,44 @@ var HelloUser = React.createClass({
 	}	
 });
 
+// Parent
+var FriendsContainer = React.createClass({
+	render: function () {
+		var name = 'Alex J. Tucker'
+		var friends = ['Ceri','Midna','me','myself','I']
+		return (
+			<div>
+				<h3> Name: </h3>
+				<h4> {name}</h4>
+				<ShowList names={friends} />
+			</div>
+		)
+	}
+});
+
+// Child of FriendsContainer
+var ShowList = React.createClass({
+	render: function () {
+		// Following is equivalent to:
+		//var listItems = friends.map(fucntion(friend){
+		//  return "<li> " + friend + "</li>";
+		//})
+		//console.log(listItems);//["<li>friend(1)</li>","<li>friend(2)</li>",...]
+		var listItems = this.props.names.map(function(friend){
+			return <li> {friend} </li>;
+		});
+		return (
+			<div>
+				<h3> Friends </h3>
+				<ul>
+					{listItems}
+				</ul>
+			</div>
+		)
+	}
+});
+
 ReactDOM.render(
-	<HelloUser name="Alex"/>,
+	<FriendsContainer />,
 	document.getElementById('app')
 );
