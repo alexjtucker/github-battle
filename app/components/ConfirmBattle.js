ee82d98c0aaa2265443ab50f404c38f809ed7d7b
alexjtucker <alexjtucker@gmail.com>
2016-03-30 09:17:22 -0400

    Implements Typo Fixes in Rendering UI Video Portion

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -3,7 +3,7 @@ var PropTypes = React.PropTypes;
 var styles = require('../styles/');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
-var UserDetailsWrapper = require('./UserDetailsWrapper/');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function puke (object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -23,12 +23,12 @@ function ConfirmBattle (props) {
           </UserDetailsWrapper>
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
-          <div className='col-sm-12' space={styles.space}>
+          <div className='col-sm-12' style={styles.space}>
             <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
               Initiate Battle!
             </button>
           </div>
-          <div className='col-sm-12' space={styles.space}>
+          <div className='col-sm-12' style={styles.space}>
             <Link to='/playerOne'>
               <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
             </Link>
